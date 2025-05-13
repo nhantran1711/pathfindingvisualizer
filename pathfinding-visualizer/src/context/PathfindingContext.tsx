9010ccd8a297889a3d46e459e8e6318b0133f40b
nhantran1711 <traty191@mymail.unisa.edu.au>
2025-05-13 13:48:35 +0930

    Set the default value for the maze Shape for the first and last tile

---
+++ pathfinding-visualizer/src/context/PathfindingContext.tsx
@@ -0,0 +1,50 @@
+// Shape of the Context value
+import type { ReactNode } from "react";
+import { Children, createContext, useState } from "react";
+import type { AlgorithmType, GridType, MazeType } from "../utls/types";
+import { createGrid } from "../utls/helpers";
+import { END_TILE_CONFIG, START_TILE_CONFIG } from "../utls/constants";
+
+interface PathfindingContextInterface {
+    // define algorithm
+    algorithm: AlgorithmType;
+    setAlgorithm: (algorithm: AlgorithmType) => void;
+
+    // define maze
+    maze: MazeType;
+    setMaze: (maze: MazeType) => void;
+
+    // define grid
+    grid: GridType;
+    setGrid: (grid: GridType) => void;
+
+    // define graph
+    isGraphVisualized: boolean;
+    setIsGraphVisualized: (isGraphVisualized: boolean) => void;
+}
+
+
+export const PathfindingContext = createContext<PathfindingContextInterface | undefined>(undefined);
+
+// Define set up
+export const PathfindingProvider = ({children} : {children: ReactNode}) => {
+    const [algorithm, setAlgorithm] = useState<AlgorithmType>("BFS"); // Set BFS to start
+    const [maze, setMaze] = useState<MazeType>("NONE"); // Maze not create yet
+    const [grid, setGrid] = useState<GridType>(createGrid(START_TILE_CONFIG, END_TILE_CONFIG)); // create shape of the grid
+    const [isGraphVisualized, setIsGraphVisualized] = useState<boolean>(false); // Set this false
+
+    return (
+        <PathfindingContext.Provider
+            value={{
+                algorithm,
+                setAlgorithm,
+                maze,
+                setMaze,
+                grid,
+                setGrid,
+                isGraphVisualized,
+                setIsGraphVisualized
+            }}
+        >{children}</PathfindingContext.Provider>
+    )
+}
--- pathfinding-visualizer/src/utls/constants.ts
+++ pathfinding-visualizer/src/utls/constants.ts
@@ -3,3 +3,28 @@
 export const MAX_ROWS = 40;
 export const MAX_COLS = 50;
 
+//  Shape of the first tile
+export const  START_TILE_CONFIG = {
+    row: 1,
+    col: 1,
+    isEnd: false,
+    isStart: false,
+    isWall: false,
+    isPath: false,
+    distance: 0,
+    isTraversed: false,
+    parent: null
+}
+
+//  Shape of the last tile
+export const  END_TILE_CONFIG = {
+    row: MAX_ROWS - 2,
+    col: MAX_COLS - 2,
+    isEnd: false,
+    isStart: false,
+    isWall: false,
+    isPath: false,
+    distance: 0,
+    isTraversed: false,
+    parent: null
+}
