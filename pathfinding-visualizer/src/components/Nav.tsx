13162c8ae93773040e8f45631f8f49ecf06e90fd
nhantran1711 <traty191@mymail.unisa.edu.au>
2025-05-17 17:08:29 +0930

    Adding a Nav bar

---
+++ pathfinding-visualizer/src/components/Nav.tsx
@@ -0,0 +1,11 @@
+export function Nav() {
+    return (
+        <div className="flex items-center justify-center min-h-[4.5rem] border-b shadow-gray-500 sm:px-4 px-0">
+            <div className="flex items-center lg:justify-between justify-center w-full sm:w-[52rem]">
+                <h1 className="lg: flex hideen w-[40px] text-2xl pl-1">
+                    Pathfinding Visualizer Project
+                </h1>
+            </div>
+        </div>
+    )
+}
